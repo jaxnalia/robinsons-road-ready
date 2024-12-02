45f15bcf1ecc4de9cb947d956a8df055fa7bf0ba
jaxnalia <jacksendall@gmail.com>
2024-12-02 23:43:47 +0000

    img

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import { Phone, ChevronRight } from "lucide-svelte";
+    import audiBack from "$lib/images/audi_back.jpeg"
 </script>
 
 <svelte:head>
@@ -9,9 +10,9 @@
 
 <section style="margin-top: 0px;height: 70vh;" class="relative overflow-clip h-screen flex flex-col justify-center text-left text-white ">
     <div class="video-docker absolute top-0 left-0 w-full h-full">
-        <video class="min-w-full min-h-full absolute object-cover"
-            src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4"
-            autoplay muted loop></video>
+        <img class="min-w-full min-h-full absolute object-cover"
+            src={audiBack}
+            alt="audi-robinsons">
     </div>
     <div class="video-content space-y-2 z-10">
         <div class="container mx-auto mt-16 mb-16">
@@ -67,7 +68,7 @@
 
 
 <style>
-    .video-docker video {
+    .video-docker img {
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
@@ -79,7 +80,7 @@
         height: 100%;
         top: 0;
         left: 0;
-        background: rgba(0, 0, 0, 0.6);
+        background: rgba(0, 0, 0, 0.5);
         z-index: 1;
     }
 </style>
