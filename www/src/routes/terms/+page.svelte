1e54fccf33e10e9fd8607337b26f4c5979bdd429
jaxnalia <jacksendall@gmail.com>
2024-12-02 22:57:04 +0000

    tnc

--- www/src/routes/terms/+page.svelte
+++ www/src/routes/terms/+page.svelte
@@ -1,62 +1,64 @@
-<div class="prose text-lg container">
+<div class="container">
+    <div class="prose text-lg">
 
-    <br>
-    <br>
-
-<h1>Terms & Conditions</h1>
-<br>
-<h3>Lesson Payments:</h3> 
-    <br>
-    Weekly lessons will be reserved once a £100 deposit has been made and held until first payment is received, which must be no less than 14 days before the first lesson begins.<br>
-    <br>
-    Driving lessons booked in advanced must be paid in full at least 14 days prior to the start date of the first lesson commencing.<br>
-    <br>
-    Lessons not paid for in full 14 days prior to beginning may be made available to other prospective pupils.<br>
-    <br>
-    Block booking allows you to reserve your lessons in advance, single pay as you go students may have to wait longer between lessons.<br>
-    <br>
-    <h3>Lesson Cancellations:</h3>
-    <br>
-    Lessons cancelled with less than 24 hours’ notice will be charged at the full rate of the lesson originally booked. All charges are at the discretion of the instructor.<br>
-    <br>
-    Lessons cancelled by the instructor in any circumstance will be re-arranged to the earliest available date convenient to both the pupil and instructor.<br>
-    <br>
-    <h3>Driving Tests:</h3>
-    <br>
-    Pupils that have booked a driving test will take priority over a pupil’s normal weekly lesson to allow a continuous flow of pupils through the learning process and open up more availability for future pupils to learn to drive.<br>
-    <br>     
-    Test should be booked following a discussion with the instructor. The instructor is in no way obliged to take a pupil to test should they book their test without an initial discussion and in addition, should the professional opinion of the instructor be that a pupil is not ready for test, the instructor can refuse to take the pupil to test.<br>
-    <br>
-    All tests booked also follow the same cancellation policy as pre-booked lessons.<br>
-    <br>
-    The instructor cannot be held responsible for test dates cancelled or changed by the DVSA. Should the test be cancelled upon arrival to the test centre then a 50% charge is still payable to the instructor for their time and car rental. In the event a cancellation occurs, some cost can be recovered directly from the DVSA. Costs cannot be recovered for poor weather. For further details, refer to the DVSA website.<br>
-    <br>
-    <h3>Alcohol & Drugs:</h3>
-    <br>
-    A zero-tolerance policy to drugs & alcohol is in place while driving tuition takes place. Should a pupil be intoxicated through drink or drugs upon attending a lesson, the pupil will be charged at the full rate of their remaining lessons and refused any future lessons.<br>
-    <br>
-    A strict no smoking policy is also in place within the vehicles.<br>
-    <br>
-    <h3>Criminal Convictions & Medical History:</h3>
-    <br>
-    Failure to disclose any criminal convictions or driving disqualifications on the initial lesson will result in the lesson being cancelled and no refund being available for any remaining lessons.<br>
-    <br>
-    In order to operate a safe driving environment, any medical history that may affect driving lessons must also be disclosed to ensure the safety of the instructor. Failing to disclose this will also result in lesson cancellation and no refund being available for any remaining lessons.<br>
-    <br>
-    Eyesight should be checked prior to learning to drive, and the pupil will be required to operate a basic eyesight check on their initial lesson, in line with the DVSA guidelines. Should a pupil fail an eyesight check, their hours will remain on account for 90 days or until they have rectified their eyesight.<br>
-    <br>
-    Refunds will not be given for failing to disclose relevant criminal convictions or medical conditions that may result in lessons being cancelled.<br>
-    <br>
-    <h3>Intensive Lessons ONLY;</h3>
-    <br>
-    A NON REFUNDABLE deposit of £200 is required upon booking to save dates, with Full payment 14 days before the start date<br>
-    <br>
-    Intensive prices include taught hours, 1.5 hours for a mock test, 3 hours for test day and DVSA test fee (Currently £62)<br>
-    <br>
-    Everyone learns at a different pace, so there is no guarantee that you will be ready at the end of the intensive course.  More hours can be booked as needed.<br>
-    Test day is at the discretion of the intensive instructor.<br>
-    <br>
-    <div class="h-20"></div>
+        <br>
+        <br>
     
-
+    <h1>Terms & Conditions</h1>
+    <br>
+    <h3>Lesson Payments:</h3> 
+        <br>
+        Weekly lessons will be reserved once a £100 deposit has been made and held until first payment is received, which must be no less than 14 days before the first lesson begins.<br>
+        <br>
+        Driving lessons booked in advanced must be paid in full at least 14 days prior to the start date of the first lesson commencing.<br>
+        <br>
+        Lessons not paid for in full 14 days prior to beginning may be made available to other prospective pupils.<br>
+        <br>
+        Block booking allows you to reserve your lessons in advance, single pay as you go students may have to wait longer between lessons.<br>
+        <br>
+        <h3>Lesson Cancellations:</h3>
+        <br>
+        Lessons cancelled with less than 24 hours’ notice will be charged at the full rate of the lesson originally booked. All charges are at the discretion of the instructor.<br>
+        <br>
+        Lessons cancelled by the instructor in any circumstance will be re-arranged to the earliest available date convenient to both the pupil and instructor.<br>
+        <br>
+        <h3>Driving Tests:</h3>
+        <br>
+        Pupils that have booked a driving test will take priority over a pupil’s normal weekly lesson to allow a continuous flow of pupils through the learning process and open up more availability for future pupils to learn to drive.<br>
+        <br>     
+        Test should be booked following a discussion with the instructor. The instructor is in no way obliged to take a pupil to test should they book their test without an initial discussion and in addition, should the professional opinion of the instructor be that a pupil is not ready for test, the instructor can refuse to take the pupil to test.<br>
+        <br>
+        All tests booked also follow the same cancellation policy as pre-booked lessons.<br>
+        <br>
+        The instructor cannot be held responsible for test dates cancelled or changed by the DVSA. Should the test be cancelled upon arrival to the test centre then a 50% charge is still payable to the instructor for their time and car rental. In the event a cancellation occurs, some cost can be recovered directly from the DVSA. Costs cannot be recovered for poor weather. For further details, refer to the DVSA website.<br>
+        <br>
+        <h3>Alcohol & Drugs:</h3>
+        <br>
+        A zero-tolerance policy to drugs & alcohol is in place while driving tuition takes place. Should a pupil be intoxicated through drink or drugs upon attending a lesson, the pupil will be charged at the full rate of their remaining lessons and refused any future lessons.<br>
+        <br>
+        A strict no smoking policy is also in place within the vehicles.<br>
+        <br>
+        <h3>Criminal Convictions & Medical History:</h3>
+        <br>
+        Failure to disclose any criminal convictions or driving disqualifications on the initial lesson will result in the lesson being cancelled and no refund being available for any remaining lessons.<br>
+        <br>
+        In order to operate a safe driving environment, any medical history that may affect driving lessons must also be disclosed to ensure the safety of the instructor. Failing to disclose this will also result in lesson cancellation and no refund being available for any remaining lessons.<br>
+        <br>
+        Eyesight should be checked prior to learning to drive, and the pupil will be required to operate a basic eyesight check on their initial lesson, in line with the DVSA guidelines. Should a pupil fail an eyesight check, their hours will remain on account for 90 days or until they have rectified their eyesight.<br>
+        <br>
+        Refunds will not be given for failing to disclose relevant criminal convictions or medical conditions that may result in lessons being cancelled.<br>
+        <br>
+        <h3>Intensive Lessons ONLY;</h3>
+        <br>
+        A NON REFUNDABLE deposit of £200 is required upon booking to save dates, with Full payment 14 days before the start date<br>
+        <br>
+        Intensive prices include taught hours, 1.5 hours for a mock test, 3 hours for test day and DVSA test fee (Currently £62)<br>
+        <br>
+        Everyone learns at a different pace, so there is no guarantee that you will be ready at the end of the intensive course.  More hours can be booked as needed.<br>
+        Test day is at the discretion of the intensive instructor.<br>
+        <br>
+        <div class="h-20"></div>
+        
+    
+    </div>
 </div>
